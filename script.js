586fdeef925c9c948ff012418c25c98872f425f7
Piotr Filipiak <kiddhkane@gmail.com>
2022-07-08 21:19:23 +0200

    Completed

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const cards = [
   },
   {
     name: "cheesburger",
-    img: "./images/cheesburger.png",
+    img: "./images/cheeseburger.png",
   },
   {
     name: "hotdog",
@@ -29,7 +29,7 @@ const cards = [
   },
   {
     name: "cheesburger",
-    img: "./images/cheesburger.png",
+    img: "./images/cheeseburger.png",
   },
   {
     name: "hotdog",
@@ -54,14 +54,59 @@ cards.sort(() => 0.5 - Math.random());
 // console.log(cards);
 
 const grid = document.querySelector(".grid");
+const resultDisplay = document.querySelector("#result");
+let cardsChosen = [];
+let cardsChosedIDs = [];
+const cardsWon = [];
 
 function createBoard() {
   for (let i = 0; i < cards.length; i++) {
     const card = document.createElement("img");
     card.setAttribute("src", "./images/blank.png");
     card.setAttribute("data-id", i);
+    card.addEventListener("click", flipCard);
     grid.append(card);
   }
 }
 
 createBoard();
+
+function checkMatch() {
+  const cards = document.querySelectorAll(".grid img");
+  const cardOneID = cardsChosedIDs[0];
+  const cardTwoID = cardsChosedIDs[1];
+
+  if (cardsChosen[0] == cardsChosen[1]) {
+    cards[cardOneID].setAttribute("src", "./images/white.png");
+    cards[cardTwoID].setAttribute("src", "./images/white.png");
+    cards[cardOneID].removeEventListener("click", flipCard);
+    cards[cardTwoID].removeEventListener("click", flipCard);
+    cardsWon.push(cardsChosen);
+  } else {
+    cards[cardOneID].setAttribute("src", "./images/blank.png");
+    cards[cardTwoID].setAttribute("src", "./images/blank.png");
+  }
+
+  if (cardOneID == cardTwoID) {
+    alert("You clicked the same card");
+    cards[cardOneID].setAttribute("src", "./images/blank.png");
+  }
+
+  cardsChosen = [];
+  cardsChosedIDs = [];
+  resultDisplay.textContent = cardsWon.length;
+  if (cardsWon.length === cards.length / 2) {
+    resultDisplay.textContent = "Congratulations!";
+  }
+}
+
+function flipCard() {
+  let cardID = this.getAttribute("data-id");
+  cardsChosen.push(cards[cardID].name);
+  cardsChosedIDs.push(cardID);
+  this.setAttribute("src", cards[cardID].img);
+
+  if (cardsChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+}
